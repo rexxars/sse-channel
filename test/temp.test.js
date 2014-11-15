bb10e7dcf1157c817e71b95b5b837b0c44278c49
Espen Hovlandsdal <rexxars@gmail.com>
2014-11-15 21:51:20 +0100

    Triggering a build

--- test/temp.test.js
+++ test/temp.test.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 describe('sse-channel', function() {
     it('works', function(done) {
-        assert.equal('works', 'works');
+        assert.equal('works', 'works', 'They should be the same');
         done();
     });
 });
