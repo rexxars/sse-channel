db5f370d52c8790c02dddc29d22a36c1159124d0
Espen Hovlandsdal <rexxars@gmail.com>
2014-11-16 18:23:48 +0100

    Simplified test server init even further

--- test/util/server-init.js
+++ test/util/server-init.js
@@ -7,10 +7,7 @@ module.exports = function(opts) {
     var channel = new Channel(opts);
 
     var server = http.createServer(function(req, res) {
-        if (req.url.indexOf(opts.path) === 0) {
-            // Serve the client using SSE-channel
-            channel.addClient(req, res);
-        }
+        channel.addClient(req, res);
     }).listen(opts.port || 6775, '127.0.0.1');
 
     return { server: server, channel: channel };
